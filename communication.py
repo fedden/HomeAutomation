fec79293c641a14011ffdbe963ac00da97aced3c
Your Name <you@example.com>
2018-04-19 15:49:58 +0000

    adding preliminary communication mesh network

---
+++ communication.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import time
+
+from mesh.links import UDPLink
+from mesh.programs import BaseProgram
+from mesh.filters import UniqueFilter
+from mesh.node import Node
+
+
+
+class ChatProgram(BaseProgram):
+    def recv(self, packet, interface):
+        print('\n>> {}'.format(packet.decode()))
+
+
+if __name__ == "__main__":
+    links = [UDPLink('en0', 2010)]
+    node = Node(links, 'me', Filters=[], Program=ChatProgram)
+    [link.start() for link in links]
+    node.start()
+
+    print("Run lan-chat.py on another laptop to talk between the two of you on en0.")
+    try:
+        while True:
+            print("------------------------------")
+            message = input('<< ')
+            node.send(bytes(message, 'UTF-8'))
+            time.sleep(0.3)
+
+    except (EOFError, KeyboardInterrupt):   # graceful CTRL-D & CTRL-C
+        node.stop()
+        [link.stop() for link in links]
